4bde46ee9c7c728e7560af783e564ffc12023b75
ZachBarkhorn <148818391+ZachBarkhorn@users.noreply.github.com>
2025-11-13 15:09:41 -0500

    Remove existing tables from connection DB script

--- updated_section_connection_db_tables.sql
+++ updated_section_connection_db_tables.sql
@@ -1,27 +1,2 @@
-DROP TABLE IF EXISTS reports;
-DROP TABLE IF EXISTS messages;
-DROP TABLE IF EXISTS users;
 
 
-CREATE TABLE users (
-    userID SERIAL PRIMARY KEY,
-    userName VARCHAR(45) NOT NULL
-);
-
-
-CREATE TABLE messages (
-    messageID SERIAL PRIMARY KEY,
-    userID INT NOT NULL REFERENCES users(userID),
-    timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
-    message VARCHAR(200),
-    chatID INT NOT NULL
-);
-
-
-CREATE TABLE reports (
-    reportID SERIAL PRIMARY KEY,
-    userID INT NOT NULL REFERENCES users(userID),
-    reportedUserID INT NOT NULL REFERENCES users(userID),
-    messageID INT NOT NULL REFERENCES messages(messageID),
-    reason VARCHAR(200)
-);
